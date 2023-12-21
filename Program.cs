547959e2453758ade06bcf7ae7bcc6717036dd20
Welzevol <148000263+Welzevol@users.noreply.github.com>
2023-12-21 21:53:17 +0300

    Task1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string PrintNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
+    return start + " " + PrintNumbers(start + 1, end);
+}
+
+Console.WriteLine($"Печать чисел от {M} до {N} = {PrintNumbers(M, N)}");
